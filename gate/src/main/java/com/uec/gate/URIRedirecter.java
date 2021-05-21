e45ac89752e080bb8cca7ff4338c194fd74416f2
TenThousand <1720224284@qq.com>
2021-05-21 14:42:31 +0800

    dev: 修复了动态路由的一些小bug

--- gate/src/main/java/com/uec/gate/URIRedirecter.java
+++ gate/src/main/java/com/uec/gate/URIRedirecter.java
@@ -2,9 +2,11 @@ package com.uec.gate;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.RouteMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -16,25 +18,36 @@ import java.util.logging.Logger;
 public class URIRedirecter implements GlobalFilter, Ordered {
 
     static Logger logger = Logger.getLogger("logger");
+    private final String GATEWAY_ROUTE = "org.springframework.cloud.gateway.support.ServerWebExchangeUtils.gatewayRoute";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
         ServerHttpRequest req = exchange.getRequest();
+
+        if(req.getPath().toString().contains("/test")) return chain.filter(exchange.mutate().build());
 
         String toLog = "\n";
 
         toLog += "uri: "+req.getURI().toString() +"\n";
 
-        URI uri = req.getURI();
-        String newUriStr = uri.getScheme()+"://"+uri.getAuthority()+uri.getPath()+"/test"+"?"+uri.getQuery();
+        URI newUri = getNewUri(req.getURI());
 
-        try {
-            uri = new URI(newUriStr);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+        Route route = exchange.getAttribute(GATEWAY_ROUTE);
+
+        if (route != null) {
+            Route newRoute = Route.async().asyncPredicate((route.getPredicate()))
+                    .filters(route.getFilters())
+                    .id(route.getId())
+                    .order(route.getOrder())
+                    .uri(newUri)
+                    .build();
+
+            exchange.getAttributes().put(GATEWAY_ROUTE,newRoute);
         }
 
-        req = req.mutate().uri(uri).build();
+        //其实在这里更新req没什么用，因为并不会根据它来进行路由
+        req = req.mutate().uri(newUri).build();
 
         toLog += "redirect to: "+req.getURI().toString() +"\n";
         logger.info(toLog);
@@ -42,8 +55,30 @@ public class URIRedirecter implements GlobalFilter, Ordered {
         return chain.filter(exchange.mutate().request(req).build());
     }
 
+
+
+    //这里有两种思路
+    //一，路由给自己(8089->8089)，改变请求中的一些参数或header，再进行二次匹配，进而转发，这本质上依然是静态的，不予采用
+    //二，改变route的uri，也就是目前采用的这种
+
+    private URI getNewUri(URI uri){
+        //在这里配置具体路由规则
+        if(!uri.getScheme().equals("http")) return uri;
+//        String newUriStr = uri.getScheme()+"://"+"localhost:8082"+uri.getPath()+"/test"+uri.getRawQuery();
+        String newUriStr = uri.getScheme()+"://"+"localhost:8082"+uri.getPath()+"?"+uri.getQuery();
+        URI newUri = null;
+        try {
+            newUri = new URI(newUriStr);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return newUri;
+    }
+
     @Override
     public int getOrder() {
         return -198;
     }
+
+
 }
